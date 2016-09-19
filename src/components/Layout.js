57b1a47c2bbb7dd2e0a9fb98e8ef638adc86428c
Tracey Trewin <traceyt@microsoft.com>
2016-09-19 16:38:17 -0700

    big old commit with everything included so it is easier for me to make progress

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Layout extends Component {
+
+    render() {
+        return (
+            <div>
+            <h1>This works!!!!</h1>
+            </div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// import '../vendor/bootstrap/css/bootstrap.min.css';
+// import '../vendor/bootstrap/css/bootstrap-theme.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
